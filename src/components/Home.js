f728382dfe84207223fd89f729c5c9dd285ce491
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-12-21 18:47:15 -0800

    initial setup

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Home.scss';
+
+export default function Home() {
+  return (
+    <div className = "homepage">
+      <h1>This is the home</h1>
+    </div>
+  )
+}

209fc502506a15a67b516304dd4478c5d305e67a
rebeccaloaustin <rebeccaloaustin@gmail.com>
2024-01-01 15:15:37 -0500

    use effect function changed

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -10,39 +10,62 @@ export default function Shop(props) {
   const [products, setProducts] = useState([]);
   const url = process.env.REACT_APP_URL
   const {get, loading} = useFetch(url)
-
+ 
   useEffect(() => {
-    fetch(`${url}/products`)
-	.then(response => response.json())
-	.then(data => setProducts(data))
-	.catch(err => console.error(err));
+    get('/products')
+      .then((data) => {
+        if (data) {
+          console.log(data);
+          setProducts(data);
+        }
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        console.log(products);
+      });
   }, []);
+  
 
 
   return (
     <div className="shoppage container">
-      {products.length > 0 &&
+      {loading && <Loader />}
+      {!loading && products.length > 0 && (
         <div className="row row-cols-1 row-cols-md-3 g-4">
           {products.map((product, index) => (
             <div className="col" key={index}>
-          <div className="card">
-                  <img
-                    src={product.image}
-                    className="card-img-top"
-                    alt={product.name}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.description}</p>
-                  <button className = "btn btn-primary" onClick={()=>{onProductAdd(product)}}> Add Product ${product.price}</button>
-                  <button className = "btn btn-danger" onClick={()=>{onProductDelete(product._id)}}> Delete Product</button>
-                  </div>
+              <div className="card">
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                      onProductAdd(product);
+                    }}
+                  >
+                    Add Product ${product.price}
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      onProductDelete(product._id);
+                    }}
+                  >
+                    Delete Product
+                  </button>
                 </div>
-    
+              </div>
             </div>
           ))}
         </div>
-}
+      )}
+      {!loading && products.length === 0 && <p>No products found.</p>}
     </div>
   );
 }

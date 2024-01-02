9aedc13fa4270d9e83808ecf5e79ad89ea0e5296
rebeccaloaustin <rebeccaloaustin@gmail.com>
2024-01-02 17:17:36 -0500

    add to cart in progress

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,18 +1,17 @@
-import React, {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../Shop.scss";
-import productData from "../products.json";
 import useFetch from "../useFetch";
 import Loader from "../Loader";
 
 export default function Shop(props) {
-  const {cart, onProductAdd , onProductDelete } = props;
+  const { cart, onProductAdd, onProductDelete } = props;
   const [products, setProducts] = useState([]);
-  const url = process.env.REACT_APP_URL
-  const {get, loading} = useFetch(url)
- 
+  const url = process.env.REACT_APP_URL;
+  const { get, loading } = useFetch(url);
+
   useEffect(() => {
-    get('/products')
+    get("/products")
       .then((data) => {
         if (data) {
           console.log(data);
@@ -24,43 +23,30 @@ export default function Shop(props) {
         console.log(products);
       });
   }, []);
-  
-
 
   return (
     <div className="shoppage container">
       {loading && <Loader />}
       {!loading && products.length > 0 && (
         <div className="row row-cols-1 row-cols-md-3 g-4">
-          {products.map((product, index) => (
-            <div className="col" key={index}>
-              <div className="card">
-                <img
-                  src={product.image}
-                  className="card-img-top"
-                  alt={product.name}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{product.name}</h5>
-                  <p className="card-text">{product.description}</p>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => {
-                      onProductAdd(product);
-                    }}
-                  >
-                    Add Product ${product.price}
-                  </button>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => {
-                      onProductDelete(product._id);
-                    }}
-                  >
-                    Delete Product
-                  </button>
+          {products.map((product) => (
+            <div className="col" key={product._id}>
+              <Link
+                to={`/product/${product._id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <div className="card">
+                  <img
+                    src={product.image}
+                    className="card-img-top"
+                    alt={product.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">{product.description}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>

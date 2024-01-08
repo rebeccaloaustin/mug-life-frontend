aa683c3cc9c034740c3452fa29d22c4f3def6557
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-01-08 15:44:36 -0800

    working on reviews

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
    <div className="container">
         <h4 className="reviewsHeader">Mug Life Brewed Experiences</h4>
         <div className="row justify-content-center"> 
-          <div className="col-sm-4 mb-4">
+          <div className="col-md-4 mb-4">
             <div className="card">
               <div className="card-block">
                 <h4 className="card-title">Exceptional Coffee Experience!</h4>
@@ -20,7 +20,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="col-sm-4 mb-4">
+          <div className="col-md-4 mb-4">
             <div className="card">
               <div className="card-block">
                 <h4 className="card-title">My Daily Brew</h4>
@@ -31,14 +31,14 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="col-sm-4 mb-4">
+          <div className="col-md-4 mb-4">
             <div className="card">
               <div className="card-block">
                 <h4 className="card-title">Impressed with First Purchase!</h4>
                 <p className="card-text">
                   "As a first-time customer, I am thoroughly impressed with the coffee I ordered from your website. The packaging was excellent, and the aroma upon opening the bag was irresistible. The flavor notes were distinct, and the overall experience exceeded my expectations. Your commitment to quality and craftsmanship has gained you a new loyal customer. Looking forward to exploring more blends!"
                 </p>
-                <div style={{color:"#969f84"}}><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-half"></i></div>
+                <div className="center-stars" style={{color:"#969f84", margin: "0 auto"}}><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-half"></i></div>
               </div>
             </div>
           </div>
